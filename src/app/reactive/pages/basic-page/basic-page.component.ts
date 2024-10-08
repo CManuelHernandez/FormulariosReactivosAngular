143832635352a9196c466638d4d3336fe14e20ad
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-08 10:04:55 +0200

    Re-establecer y establecer valor al formulario

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -6,11 +6,17 @@ import {
   Validators,
 } from '@angular/forms';
 
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 6,
+};
+
 @Component({
   selector: 'app-basic-page',
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
   /* public myForm: FormGroup = new FormGroup({
     name: new FormControl('', [], []),
     price: new FormControl(0, [], []),
@@ -25,8 +31,14 @@ export class BasicPageComponent {
     inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
+  ngOnInit(): void {
+    this.myForm.reset(rtx5090);
+  }
+
   onSave(): void {
     if (this.myForm.invalid) return;
     console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 0, inStorage: 0 });
   }
 }
